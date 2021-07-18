13c62279cc34a5c84e5e8ad35b48f472dcf184cf
Kumar Saptam <saptampro2003@gmail.com>
2021-07-18 20:04:56 +0530

    Started working on admin panel dashboard

--- src/app/ap-dashboard/ap-dashboard.component.ts
+++ src/app/ap-dashboard/ap-dashboard.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ap-dashboard.component.scss'],
 })
 export class APDashboardComponent implements OnInit {
-
-  constructor() { }
+  totalOrders: number=24;
+  totalCancelled: number=352;
+  totalItems: number=450;
+  growthRate: number=45;  
+  constructor() { } 
 
   ngOnInit() {}
 
