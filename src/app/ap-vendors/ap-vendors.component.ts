fb1de5e760ad7c3d68c3e7ed83b1b3e1cfdf439a
Kumar Saptam <saptampro2003@gmail.com>
2021-07-20 12:43:24 +0530

    Admin Panel's Vendors section is complete

--- src/app/ap-vendors/ap-vendors.component.ts
+++ src/app/ap-vendors/ap-vendors.component.ts
@@ -6,7 +6,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ap-vendors.component.scss'],
 })
 export class APVendorsComponent implements OnInit {
-
+  vendors=[
+    {
+      "image":"https://i.pravatar.cc/250",
+      "name":"Complete Painting",
+      "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus tincidunt commodo. Nunc justo nisi, vestibulum facilisis porta vestibulum, ultrices volutpat arcu. Quisque nec dui mattis, fringilla magna in, vulputate enim. Fusce ut euismod ligula, id laoreet ex. ",
+      "totalProducts":"543",
+      "totalCancelled":"43",
+      "totalSold":"32153",
+      "totalReturned":"31"
+    },
+    {
+      "image":"https://i.pravatar.cc/250",
+      "name":"Complete Painting",
+      "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus tincidunt commodo. Nunc justo nisi, vestibulum facilisis porta vestibulum, ultrices volutpat arcu. Quisque nec dui mattis, fringilla magna in, vulputate enim. Fusce ut euismod ligula, id laoreet ex. ",
+      "totalProducts":"543",
+      "totalCancelled":"43",
+      "totalSold":"32153",
+      "totalReturned":"31"
+    },
+    {
+      "image":"https://i.pravatar.cc/250",
+      "name":"Complete Painting",
+      "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus tincidunt commodo. Nunc justo nisi, vestibulum facilisis porta vestibulum, ultrices volutpat arcu. Quisque nec dui mattis, fringilla magna in, vulputate enim. Fusce ut euismod ligula, id laoreet ex. ",
+      "totalProducts":"543",
+      "totalCancelled":"43",
+      "totalSold":"32153",
+      "totalReturned":"31"
+    },
+    {
+      "image":"https://i.pravatar.cc/250",
+      "name":"Complete Painting",
+      "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus tincidunt commodo. Nunc justo nisi, vestibulum facilisis porta vestibulum, ultrices volutpat arcu. Quisque nec dui mattis, fringilla magna in, vulputate enim. Fusce ut euismod ligula, id laoreet ex. ",
+      "totalProducts":"543",
+      "totalCancelled":"43",
+      "totalSold":"32153",
+      "totalReturned":"31"
+    },
+    {
+      "image":"https://i.pravatar.cc/250",
+      "name":"Complete Painting",
+      "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus tincidunt commodo. Nunc justo nisi, vestibulum facilisis porta vestibulum, ultrices volutpat arcu. Quisque nec dui mattis, fringilla magna in, vulputate enim. Fusce ut euismod ligula, id laoreet ex. ",
+      "totalProducts":"543",
+      "totalCancelled":"43",
+      "totalSold":"32153",
+      "totalReturned":"31"
+    },
+    {
+      "image":"https://i.pravatar.cc/250",
+      "name":"Complete Painting",
+      "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus tincidunt commodo. Nunc justo nisi, vestibulum facilisis porta vestibulum, ultrices volutpat arcu. Quisque nec dui mattis, fringilla magna in, vulputate enim. Fusce ut euismod ligula, id laoreet ex. ",
+      "totalProducts":"543",
+      "totalCancelled":"43",
+      "totalSold":"32153",
+      "totalReturned":"31"
+    },
+    {
+      "image":"https://i.pravatar.cc/250",
+      "name":"Complete Painting",
+      "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus tincidunt commodo. Nunc justo nisi, vestibulum facilisis porta vestibulum, ultrices volutpat arcu. Quisque nec dui mattis, fringilla magna in, vulputate enim. Fusce ut euismod ligula, id laoreet ex. ",
+      "totalProducts":"543",
+      "totalCancelled":"43",
+      "totalSold":"32153",
+      "totalReturned":"31"
+    },
+    
+  ]
   constructor() { }
 
   ngOnInit() {}
