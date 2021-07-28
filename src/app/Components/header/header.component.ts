0ec8ac38e9834c3d4098322b2c2ab198c9e9a712
Kumar Saptam <saptampro2003@gmail.com>
2021-07-28 11:47:49 +0530

    Re-Done the header

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
+  image="https://i.pravatar.cc/300"
   constructor() { }
 
   ngOnInit() {}
--- src/app/customerPanel/auth-login/auth-login.component.ts
+++ src/app/customerPanel/auth-login/auth-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataProvider } from 'src/app/providers/data.provider';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-auth-login',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthLoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService:AuthService,public dataProvider:DataProvider) { }
 
   ngOnInit() {}
 
