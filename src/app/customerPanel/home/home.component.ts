b8d240b7de4211e3ce23e90a11dd21f569f8e97f
Kumar Saptam <saptampro2003@gmail.com>
2021-08-08 17:13:05 +0530

    Half connected frontend

--- src/app/customerPanel/home/home.component.ts
+++ src/app/customerPanel/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import * as AOS from 'aos';
 @Component({
   selector: 'app-home',
@@ -7,9 +8,120 @@ import * as AOS from 'aos';
 })
 export class HomeComponent implements OnInit {
 screenwidth=window.innerWidth
-  constructor() { }
-
-  ngOnInit() {AOS.init();}
+  constructor(public afs: AngularFirestore,) { }
+  allHandmadeProds=[];
+  allDigitalProds=[];
+  allRecommendedProds=[];
+  allProducts=[];
+  shuffle(array) {
+    var currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  ngOnInit() {
+    AOS.init();
+    this.afs.collection('data').doc("productData").valueChanges().subscribe((value:any)=>{
+      this.allHandmadeProds=[];
+      value.categories.forEach((element:any) => {
+        // console.log(value.categories.indexOf(element),"indexof")
+        if (value.categories.indexOf(element)==0){
+          // console.log("removing elements");
+          this.allHandmadeProds.length=0;
+          // console.log(this.allProds.length)
+        }
+        this.afs.collection('products').doc('Manual Artworks')
+        .collection('categories')
+        .doc(element.subCategory)
+        .collection('products').valueChanges().subscribe((proddata)=>{
+          this.allProducts[element.category] = proddata;
+          proddata.forEach((product:any) => {
+            let unknown = 0;
+            this.allHandmadeProds.forEach((oldProduct:any) => {
+              if (product.productId==oldProduct.productId) {
+                console.log("already exists");
+                unknown++;
+              }
+            })
+            if (unknown==0) {
+              this.allHandmadeProds.push(product);
+            }
+          })
+        })
+      })
+      
+    });
+    this.afs.collection('data').doc("productData").valueChanges().subscribe((value:any)=>{
+      this.allDigitalProds=[];
+      value.categories.forEach((element:any) => {
+        // console.log(value.categories.indexOf(element),"indexof")
+        if (value.categories.indexOf(element)==0){
+          // console.log("removing elements");
+          this.allDigitalProds.length=0;
+          // console.log(this.allProds.length)
+        }
+        this.afs.collection('products').doc('Digital Artworks')
+        .collection('categories')
+        .doc(element.subCategory)
+        .collection('products').valueChanges().subscribe((proddata)=>{
+          this.allProducts[element.category] = proddata;
+          proddata.forEach((product:any) => {
+            let unknown = 0;
+            this.allDigitalProds.forEach((oldProduct:any) => {
+              if (product.productId==oldProduct.productId) {
+                console.log("already exists");
+                unknown++;
+              }
+            })
+            if (unknown==0) {
+              this.allDigitalProds.push(product);
+            }
+          })
+        })
+      })
+    });
+    this.afs.collection('data').doc("productData").valueChanges().subscribe((value:any)=>{
+      this.allRecommendedProds=[];
+      value.categories.forEach((element:any) => {
+        // console.log(value.categories.indexOf(element),"indexof")
+        if (value.categories.indexOf(element)==0){
+          // console.log("removing elements");
+          this.allRecommendedProds.length=0;
+          // console.log(this.allProds.length)
+        }
+        this.afs.collection('products').doc(element.category)
+        .collection('categories')
+        .doc(element.subCategory)
+        .collection('products').valueChanges().subscribe((proddata)=>{
+          this.allProducts[element.category] = proddata;
+          proddata.forEach((product:any) => {
+            let unknown = 0;
+            this.allRecommendedProds.forEach((oldProduct:any) => {
+              if (product.productId==oldProduct.productId) {
+                console.log("already exists");
+                unknown++;
+              }
+            })
+            if (unknown==0) {
+              this.allRecommendedProds.push(product);
+            }
+          })
+        })
+      })
+      this.allRecommendedProds=this.shuffle(this.allRecommendedProds)
+    });
+  }
   products=[
     {
       "img":"https://source.unsplash.com/940x650",
--- src/app/customerPanel/singleproduct/singleproduct.component.ts
+++ src/app/customerPanel/singleproduct/singleproduct.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-singleproduct',
@@ -6,149 +7,183 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./singleproduct.component.scss'],
 })
 export class SingleproductComponent implements OnInit {
-    screenwidth=window.innerWidth
-  constructor() { }
+  screenwidth = window.innerWidth;
+    category:string;
+    subcategory:string;
+    productId:string;
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.queryParams.subscribe(params => {
+        this.category = params['category'];
+        this.subcategory = params['subcategory']
+        this.productId = params['productId'];
+    });
+  }
 
   ngOnInit(): void {
-    const product_filters = document.getElementById('product-filters') as HTMLElement;
-    const product_types_container = document.getElementById('product-types-container') as HTMLElement;
-    const product_types_title = document.getElementById('product-types-title') as HTMLElement;
-    const product_types = document.getElementById('product-types') as HTMLElement;
+    const product_filters = document.getElementById(
+      'product-filters'
+    ) as HTMLElement;
+    const product_types_container = document.getElementById(
+      'product-types-container'
+    ) as HTMLElement;
+    const product_types_title = document.getElementById(
+      'product-types-title'
+    ) as HTMLElement;
+    const product_types = document.getElementById(
+      'product-types'
+    ) as HTMLElement;
 
-    product_filters?.addEventListener('click', (event) => {
+    product_filters?.addEventListener(
+      'click',
+      (event) => {
         const targ = event.target as HTMLElement;
         if (targ.classList.contains('product-filter')) {
-            const filter_id = targ.id;
-            const filter_type = filter_id.substring(0, filter_id.indexOf('-'));
+          const filter_id = targ.id;
+          const filter_type = filter_id.substring(0, filter_id.indexOf('-'));
 
-            var n = 0, type_image_links : string[] = [], type_names : string[] = [];
+          var n = 0,
+            type_image_links: string[] = [],
+            type_names: string[] = [];
 
-            switch (filter_type) {
-                case 'size':
-                    n = 4;
-                    type_image_links = [
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                    ];
-                    type_names = [
-                        'type Option 1',
-                        'type Option 2',
-                        'type Option 3',
-                        'type Option 4',
-                    ];
-                    break;
+          switch (filter_type) {
+            case 'size':
+              n = 4;
+              type_image_links = [
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+              ];
+              type_names = [
+                'type Option 1',
+                'type Option 2',
+                'type Option 3',
+                'type Option 4',
+              ];
+              break;
 
-                case 'frame':
-                    n = 4;
-                    type_image_links = [
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                    ];
-                    type_names = [
-                        'Frame Option 1',
-                        'Frame Option 2',
-                        'Frame Option 3',
-                        'Frame Option 4',
-                    ];
-                    break;
+            case 'frame':
+              n = 4;
+              type_image_links = [
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+              ];
+              type_names = [
+                'Frame Option 1',
+                'Frame Option 2',
+                'Frame Option 3',
+                'Frame Option 4',
+              ];
+              break;
 
-                case 'color':
-                    n = 4;
-                    type_image_links = [
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                    ];
-                    type_names = [
-                        'Color Option 1',
-                        'Color Option 2',
-                        'Color Option 3',
-                        'Color Option 4',
-                    ];
-                    break;
-                    type_image_links = [
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                    ];
-                    type_names = [
-                        'Color Option 1',
-                        'Color Option 2',
-                        'Color Option 3',
-                        'Color Option 4',
-                    ];
-                    break;
+            case 'color':
+              n = 4;
+              type_image_links = [
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+              ];
+              type_names = [
+                'Color Option 1',
+                'Color Option 2',
+                'Color Option 3',
+                'Color Option 4',
+              ];
+              break;
+              type_image_links = [
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+              ];
+              type_names = [
+                'Color Option 1',
+                'Color Option 2',
+                'Color Option 3',
+                'Color Option 4',
+              ];
+              break;
 
-                case 'quantity':
-                    n = 4;
-                    n = 4;
-                    type_image_links = [
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                        'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-                    ];
-                    type_names = [
-                        'Frame Option 1',
-                        'Frame Option 2',
-                        'Frame Option 3',
-                        'Frame Option 4',
-                    ];
-                    break;
-            }
+            case 'quantity':
+              n = 4;
+              n = 4;
+              type_image_links = [
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+              ];
+              type_names = [
+                'Frame Option 1',
+                'Frame Option 2',
+                'Frame Option 3',
+                'Frame Option 4',
+              ];
+              break;
+          }
 
-            product_types.innerHTML = '';
+          product_types.innerHTML = '';
 
-            for (var i = 0; i < n; i++) {
-                product_types.innerHTML += `
+          for (var i = 0; i < n; i++) {
+            product_types.innerHTML +=
+              `
                     <div class="product-type" style="width: 140px;
                     margin: 0 .5em;">
-                        <img src="` + type_image_links[i] + `" class="product-type-image">
+                        <img src="` +
+              type_image_links[i] +
+              `" class="product-type-image">
                         <p class="product-type-name" style="text-align: center;
                         margin-top: .5em;
                         font-size: 1.1rem;
-                        font-family: 'Poppins';">` + type_names[i] + `</p>
+                        font-family: 'Poppins';">` +
+              type_names[i] +
+              `</p>
                     </div>
                 `;
-            }
+          }
 
-            product_types_title.innerHTML = filter_type.charAt(0).toUpperCase() + filter_type.substring(1) + ' Types';
+          product_types_title.innerHTML =
+            filter_type.charAt(0).toUpperCase() +
+            filter_type.substring(1) +
+            ' Types';
 
-            product_types_container.style.display = 'block';
+          product_types_container.style.display = 'block';
         }
-    }, false);
-
-}
-  products=[
+      },
+      false
+    );
+  }
+  products = [
     {
-      "img":"https://source.unsplash.com/940x650",
-      "name":"ArtWork Product",
-      "description":"Lorem ipsum dolor sit amet, consecteturadipiscing elit. Curabitur cursus tinciduntcommodo. Nunc justo nisi, vestibulum.",
-      "price":"2300",
+      img: 'https://source.unsplash.com/940x650',
+      name: 'ArtWork Product',
+      description:
+        'Lorem ipsum dolor sit amet, consecteturadipiscing elit. Curabitur cursus tinciduntcommodo. Nunc justo nisi, vestibulum.',
+      price: '2300',
     },
     {
-      "img":"https://source.unsplash.com/940x650",
-      "name":"ArtWork Product",
-      "description":"Lorem ipsum dolor sit amet, consecteturadipiscing elit. Curabitur cursus tinciduntcommodo. Nunc justo nisi, vestibulum.",
-      "price":"2300",
+      img: 'https://source.unsplash.com/940x650',
+      name: 'ArtWork Product',
+      description:
+        'Lorem ipsum dolor sit amet, consecteturadipiscing elit. Curabitur cursus tinciduntcommodo. Nunc justo nisi, vestibulum.',
+      price: '2300',
     },
     {
-      "img":"https://source.unsplash.com/940x650",
-      "name":"ArtWork Product",
-      "description":"Lorem ipsum dolor sit amet, consecteturadipiscing elit. Curabitur cursus tinciduntcommodo. Nunc justo nisi, vestibulum.",
-      "price":"2300",
+      img: 'https://source.unsplash.com/940x650',
+      name: 'ArtWork Product',
+      description:
+        'Lorem ipsum dolor sit amet, consecteturadipiscing elit. Curabitur cursus tinciduntcommodo. Nunc justo nisi, vestibulum.',
+      price: '2300',
     },
     {
-      "img":"https://source.unsplash.com/940x650",
-      "name":"ArtWork Product",
-      "description":"Lorem ipsum dolor sit amet, consecteturadipiscing elit. Curabitur cursus tinciduntcommodo. Nunc justo nisi, vestibulum.",
-      "price":"2300",
+      img: 'https://source.unsplash.com/940x650',
+      name: 'ArtWork Product',
+      description:
+        'Lorem ipsum dolor sit amet, consecteturadipiscing elit. Curabitur cursus tinciduntcommodo. Nunc justo nisi, vestibulum.',
+      price: '2300',
     },
-  ]
+  ];
 }
